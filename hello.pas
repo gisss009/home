fc9f128fec7dafc981ba8915af7b339d4739266c
Виктория Горбанева <viktoryagorbaneva@yandex.ru>
2023-10-08 17:54:22 +0300

    Приветствие на разных языках

---
+++ hello.pas
@@ -0,0 +1,14 @@
+﻿begin
+  print('what language do you speak?');
+  var a := readstring;
+  if a = 'russian' then
+    print('Здравствуйте!');
+  if a = 'english' then
+    print('Hello!');
+  if a = 'chinese' then
+    print('你好！');
+  if a = 'spanish' then
+    print('¡Hola!');
+  if a = 'italian' then
+    print('Ciao!')
+end.
